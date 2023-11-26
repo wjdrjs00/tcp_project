0543340feb1d0dba9f38be4da48e3b8d78a4276a
wjdrjs00 <kimjk3409@gmail.com>
2023-11-26 11:09:21 +0900

    Update UserDTO.java

--- yeongyeoldoli/src/main/java/user/UserDTO.java
+++ yeongyeoldoli/src/main/java/user/UserDTO.java
@@ -9,10 +9,6 @@ public class UserDTO {
     private String userBirthDate;
     private String userType;
     
-	/*
-	 * private String postcode; private String roadAddress; private String
-	 * jibunAddress; private String detailAddress; private String extraAddress;
-	 */
     
 	public String getUserID() {
 		return userID;
@@ -51,18 +47,6 @@ public class UserDTO {
 		this.userBirthDate = userBirthDate;
 	}
 
-	/*
-	 * public String getPostcode() { return postcode; } public void
-	 * setPostcode(String postcode) { this.postcode = postcode; } public String
-	 * getRoadAddress() { return roadAddress; } public void setRoadAddress(String
-	 * roadAddress) { this.roadAddress = roadAddress; } public String
-	 * getJibunAddress() { return jibunAddress; } public void setJibunAddress(String
-	 * jibunAddress) { this.jibunAddress = jibunAddress; } public String
-	 * getDetailAddress() { return detailAddress; } public void
-	 * setDetailAddress(String detailAddress) { this.detailAddress = detailAddress;
-	 * } public String getExtraAddress() { return extraAddress; } public void
-	 * setExtraAddress(String extraAddress) { this.extraAddress = extraAddress; }
-	 */
 	public String getUserType() {
 		return userType;
 	}
@@ -74,8 +58,6 @@ public class UserDTO {
 	
     public UserDTO(String userID, String userPassword, String userEmail, String userPhoneNumber,
             String userName, String userBirthDate, String userType
-            //String postcode, String roadAddress,
-            //String jibunAddress, String detailAddress, String extraAddress, String userType
             ) {
 	 this.userID = userID;
 	 this.userPassword = userPassword;
@@ -83,11 +65,6 @@ public class UserDTO {
 	 this.userPhoneNumber = userPhoneNumber;
 	 this.userName = userName;
 	 this.userBirthDate = userBirthDate;
-		/*
-		 * this.postcode = postcode; this.roadAddress = roadAddress; this.jibunAddress =
-		 * jibunAddress; this.detailAddress = detailAddress; this.extraAddress =
-		 * extraAddress;
-		 */
 	 this.userType = userType;
 	}
 }
